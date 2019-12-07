8df99490ad0e974157d86e25848c970bc38382ea
Michelle Yuan <michelleyuan@Michelles-MacBook-Air.local>
2019-12-06 22:57:03 -0800

    Day 2 Part 2

--- day_2/intcode.py
+++ day_2/intcode.py
@@ -10,16 +10,32 @@ def main():
         sys.exit("Error: file '%s' not found" % sys.argv[1])
 
     f = open(filename)
-
     program_str = f.read().split(",")
-    program = map(int, program_str)
-
     f.close()
 
-    program[1] = 12
-    program[2] = 2
+    program = map(int, program_str)
+    program_cp = list(program)
+
+    run_intcode(program_cp, 12, 2)
+
+    print program_cp[0]
+
+    for noun in xrange(0,100):
+        for verb in xrange(0, 100):
+            program_cp = list(program)
+
+            run_intcode(program_cp, noun, verb)
+
+            if program_cp[0] == 19690720:
+                print 100 * noun + verb
+                break
+
+def run_intcode(program, noun, verb):
+    program[1] = noun
+    program[2] = verb
 
     position = 0
+
     while position < len(program):
         if program[position] == 99:
             break
@@ -29,7 +45,7 @@ def main():
             program[program[position + 3]] = program[program[position+1]] * program[program[position+2]]
         position += 4
 
-    print program[0]
+
 
 if __name__ == "__main__":
     main()
